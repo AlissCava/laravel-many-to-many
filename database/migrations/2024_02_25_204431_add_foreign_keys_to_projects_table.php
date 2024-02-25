b38fe8e651526dcfa6a32b97cf015bb4d498e10d
AlissCava <cavalleraalissa@gmail.com>
2024-02-25 21:56:36 +0100

    passo 4 e 5

--- database/migrations/2024_02_25_204431_add_foreign_keys_to_projects_table.php
+++ database/migrations/2024_02_25_204431_add_foreign_keys_to_projects_table.php
@@ -15,8 +15,6 @@ return new class extends Migration
     {
         Schema::table('projects', function (Blueprint $table) {
             $table -> foreign('user_id') -> references('id') -> on('users') -> onDelete('cascade');
-            }
-            )
         });
     }
 
