0b21fd5702445ecef648a88e4b927ad226211573
AlissCava <cavalleraalissa@gmail.com>
2024-02-28 15:52:55 +0100

    passaggio 7 e 8

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -6,7 +6,26 @@ use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    $projects = Project::with('technologies')->get();
 
-    return view('projects.index', compact('projects'));
+     public function create()
+    {
+        $users = User::all();
+        $technologies = Technology::all();
+
+        return view('projects.create', compact('users', 'technologies'));
+    }
+
+    public function store(Request $request)
+    {
+        $project = Project::create($request->all());
+        $project->technologies()->attach($request->technologies);
+
+        return redirect()->route('projects.index');
+    }
+
+    public function edit(Project $project)
+    {
+        $users = User::all();
+        $technologies = Technology
+    }
 }
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,5 +3,23 @@
     <title>Home</title>
 @endsection
 @section('content')
-    <h1>Hello, World!</h1>
+    
+    @foreach ($projects as $project)
+    <tr>
+        <td>{{ $project->id }}</td>
+        <td>{{ $project->name }}</td>
+        <td>{{ $project->user->name }}</td>
+        <td>
+            @foreach ($project->technologies as $technology)
+                {{ $technology->name }} 
+            @endforeach
+        </td>
+        <td>
+            <a href="{{ route('projects.edit', $project) }}">Modifica</a>
+            <a href="{{ route('projects.destroy', $project) }}">Elimina</a>
+        </td>
+    </tr>
+@endforeach
+
+
 @endsection
